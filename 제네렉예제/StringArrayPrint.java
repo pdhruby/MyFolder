2bf290339cb7b4df1ef4eba4c5a401c8ae094d0c
pdh49 <102115788+pdh49@users.noreply.github.com>
2022-05-20 17:36:38 +0900

    Delete 제네렉예제 directory

--- 제네렉예제/StringArrayPrint.java
+++
@@ -1,9 +0,0 @@
-package kr.human.util;
-
-public class StringArrayPrint {
-	public static void print(String[] array) {
-		System.out.print(array.length + "개 : ");
-		for(String i : array) System.out.print( i + " ");
-		System.out.println();
-	}
-}
